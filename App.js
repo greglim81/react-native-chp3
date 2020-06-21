afee098548a3255716a514f24d1006c7110490dc
Greg Lim <limjunqu@gmail.com>
2020-06-21 12:01:07 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import ProductList from './ProductList';
+import Rating from './Rating';
+import { Container, Header, Content, Button, Text } from 'native-base';
 
 export default function App() {
 
+  const isValid = true;
+
   return (
-    <ProductList />
+    <Container>
+      <Header />
+      <Content>
+        <Rating rating='1' />
+        <Rating rating='2' />
+        <Rating rating='3' />
+        <Rating rating='4' />
+        <Rating rating='5' />   
+      </Content>
+    </Container>
   );
 }
