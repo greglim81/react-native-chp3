4643cf15509c48f013539277e5bcfd2c0c53434b
Greg Lim <limjunqu@gmail.com>
2020-06-21 12:02:42 +0800

    Update Rating.js

--- Rating.js
+++ Rating.js
@@ -1,21 +1,61 @@
-import React from 'react';
-import Rating from './Rating';
-import { Container, Header, Content, Button, Text } from 'native-base';
+import React, { useState } from 'react';
+import { TouchableWithoutFeedback } from "react-native";
+import { H1, Icon } from 'native-base';
+import { Row } from 'react-native-easy-grid';
 
-export default function App() {
+export default function Rating(props) {
 
-  const isValid = true;
+    const [rating, setRating] = useState(props.rating)
 
-  return (
-    <Container>
-      <Header />
-      <Content>
-        <Rating rating='1' />
-        <Rating rating='2' />
-        <Rating rating='3' />
-        <Rating rating='4' />
-        <Rating rating='5' />   
-      </Content>
-    </Container>
-  );
+    return ( 
+        <Row>      
+            <H1>Rating: {rating}</H1>           
+            
+            {rating >= 1 ? ( 
+                <TouchableWithoutFeedback onPress={() => setRating(1)}>
+                    <Icon type="FontAwesome" name="star" />
+                </TouchableWithoutFeedback>
+            ) : (         
+                <TouchableWithoutFeedback onPress={() => setRating(1)}>
+                    <Icon type="FontAwesome" name="star-o" />   
+                </TouchableWithoutFeedback>            
+            )}  
+            {rating >= 2 ? ( 
+                <TouchableWithoutFeedback onPress={() => setRating(2)}>
+                    <Icon type="FontAwesome" name="star" />
+                </TouchableWithoutFeedback>
+            ) : (         
+                <TouchableWithoutFeedback onPress={() => setRating(2)}>
+                    <Icon type="FontAwesome" name="star-o" />   
+                </TouchableWithoutFeedback>         
+            )}  
+            {rating >= 3 ? ( 
+                <TouchableWithoutFeedback onPress={() => setRating(3)}>
+                    <Icon type="FontAwesome" name="star" />
+                </TouchableWithoutFeedback>        
+            ) : (         
+                <TouchableWithoutFeedback onPress={() => setRating(3)}>
+                    <Icon type="FontAwesome" name="star-o" />   
+                </TouchableWithoutFeedback>         
+            )}  
+            {rating >= 4 ? ( 
+                <TouchableWithoutFeedback onPress={() => setRating(4)}>
+                    <Icon type="FontAwesome" name="star" />
+                </TouchableWithoutFeedback>
+            ) : (         
+                <TouchableWithoutFeedback onPress={() => setRating(4)}>
+                    <Icon type="FontAwesome" name="star-o" />   
+                </TouchableWithoutFeedback>         
+            )}  
+            {rating >= 5 ? ( 
+                <TouchableWithoutFeedback onPress={() => setRating(5)}>
+                    <Icon type="FontAwesome" name="star" />
+                </TouchableWithoutFeedback>        
+            ) : (         
+                <TouchableWithoutFeedback onPress={() => setRating(5)}>
+                    <Icon type="FontAwesome" name="star-o" />   
+                </TouchableWithoutFeedback>         
+            )}                                            
+        </Row>
+      );
 }
